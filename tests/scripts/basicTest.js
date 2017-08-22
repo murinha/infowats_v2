fc2382944619377cf362b8aa863f950a7654560f
Vanderson de Freitas Silva <vanfreit@br.ibm.com>
2017-08-22 08:12:21 -0500

    Updating Test Case 1

--- tests/scripts/basicTest.js
+++ tests/scripts/basicTest.js
@@ -2,11 +2,9 @@ module.exports = {
   'Demo test GM' : function (browser) {
     browser
       .url('http://infowats-v2-test.mybluemix.net')
-      .waitForElementVisible('.page-top', 60000, true,
-        function(){}, 'Waiting Page Header to become visible')
-      .setValue('.page-top', 'Test 1')
-      .waitForElementPresent('#page-top', 60000)
-      .pause(5000)
+      .waitForElementVisible('body', 60000, true,
+        function(){}, 'Waiting body to become visible')
+      .assert.title('Infowats')
       .end();
   }
 };
