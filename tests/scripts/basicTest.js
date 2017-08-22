831979f214d6e6a4192488d98c9337b36015cec5
Vanderson de Freitas Silva <vanfreit@br.ibm.com>
2017-08-22 13:02:15 +0000

    Creating Test Case 1

--- tests/scripts/basicTest.js
+++ tests/scripts/basicTest.js
@@ -1,11 +1,11 @@
 module.exports = {
   'Demo test GM' : function (browser) {
     browser
-      .url('https://ibmmethod-beta.mybluemix.net/devops/method')
-      .waitForElementVisible('.searchbox.hidden-xs.hidden-sm', 60000, true,
-        function(){}, 'Waiting for Search Input to become visible')
-      .setValue('.searchbox.hidden-xs.hidden-sm', 'Test')
-      .waitForElementPresent('#search-page', 60000)
+      .url('http://infowats-v2-test.mybluemix.net')
+      .waitForElementVisible('.page-top', 60000, true,
+        function(){}, 'Waiting Page Header to become visible')
+      .setValue('.page-top', 'Test 1')
+      .waitForElementPresent('#page-top', 60000)
       .pause(5000)
       .end();
   }
