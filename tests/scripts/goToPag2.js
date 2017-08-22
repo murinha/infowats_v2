6dca204fd31c447f66105149f061bbc99d28a915
Vanderson de Freitas Silva <vanfreit@br.ibm.com>
2017-08-22 08:25:19 -0500

    Adding # on Button ID

--- tests/scripts/goToPag2.js
+++ tests/scripts/goToPag2.js
@@ -4,8 +4,8 @@ module.exports = {
       .url('http://infowats-v2-test.mybluemix.net')
       .waitForElementVisible('body', 60000, true,
         function(){}, 'Waiting body to become visible')
-      .assert.visible('onboarding-btn')
-      .click('onboarding-btn')
+      .assert.visible('#onboarding-btn')
+      .click('#onboarding-btn')
       .end();
   }
 };
